7262cb97c11b49d70b685262c8579c9fcbda9a35
Mauricio Rojas <mrojasb2000@gmail.com>
2025-11-29 18:47:37 -0300

    chore: enable cors

--- backend/my-inventory-app/src/main/java/com/bramworks/inventory/controller/CategoryController.java
+++ backend/my-inventory-app/src/main/java/com/bramworks/inventory/controller/CategoryController.java
@@ -9,6 +9,7 @@ import com.bramworks.inventory.model.Category;
 import com.bramworks.inventory.responses.ResponseRest;
 import com.bramworks.inventory.services.CategoryService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryController {
