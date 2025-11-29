66e14f0573698aaf5f92ea1fdd2a930c0ffd7216
Mauricio Rojas <mrojasb2000@gmail.com>
2025-11-29 17:10:37 -0300

    chore: add get category by id

--- backend/my-inventory-app/src/main/java/com/bramworks/inventory/services/CategoryServiceImpl.java
+++ backend/my-inventory-app/src/main/java/com/bramworks/inventory/services/CategoryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.bramworks.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +27,26 @@ public class CategoryServiceImpl implements CategoryService {
         List<Category> categories = (List<Category>) categoryRepository.findAll();
         ResponseRest response = new ResponseRest();
         
+        response.getCategoryResponse().setCategories(categories);
+        response.setMetadata("Result.ok", "00", "Categories retrieved successfully");
+        
+        return ResponseEntity.ok(response);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ResponseEntity<ResponseRest> getById(Long id) {
+        ResponseRest response = new ResponseRest();
+        List<Category> categories = new ArrayList<>();
+
+        Optional<Category> category = categoryRepository.findById(id);
+       
+        if (category.isEmpty()) {
+            response.setMetadata("Result.error", "99", "Category not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+
+        categories.add(category.get());
         response.getCategoryResponse().setCategories(categories);
         response.setMetadata("Result.ok", "00", "Categories retrieved successfully");
         
