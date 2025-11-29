c0700e1bbee6d731aef32783270221fcbcd7d19a
Mauricio Rojas <mrojasb2000@gmail.com>
2025-11-29 17:16:35 -0300

    chore: add operation log

--- backend/my-inventory-app/src/main/java/com/bramworks/inventory/services/CategoryServiceImpl.java
+++ backend/my-inventory-app/src/main/java/com/bramworks/inventory/services/CategoryServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +18,10 @@ import com.bramworks.inventory.responses.ResponseRest;
 
 
 @Service
+
 public class CategoryServiceImpl implements CategoryService {
+
+    private static final Logger log = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -29,7 +34,7 @@ public class CategoryServiceImpl implements CategoryService {
         
         response.getCategoryResponse().setCategories(categories);
         response.setMetadata("Result.ok", "00", "Categories retrieved successfully");
-        
+        log.info("Categories retrieved successfully");
         return ResponseEntity.ok(response);
     }
 
@@ -42,6 +47,7 @@ public class CategoryServiceImpl implements CategoryService {
         Optional<Category> category = categoryRepository.findById(id);
        
         if (category.isEmpty()) {
+            log.error("Category not found with id: {}", id);
             response.setMetadata("Result.error", "99", "Category not found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
@@ -49,7 +55,7 @@ public class CategoryServiceImpl implements CategoryService {
         categories.add(category.get());
         response.getCategoryResponse().setCategories(categories);
         response.setMetadata("Result.ok", "00", "Categories retrieved successfully");
-        
+        log.info("Category retrieved successfully with id: {}", id);
         return ResponseEntity.ok(response);
     }
 }
