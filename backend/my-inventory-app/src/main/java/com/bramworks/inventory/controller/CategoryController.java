2d61f457471f9a6ea601748978eeb49a6ffd68b5
Mauricio Rojas <mrojasb2000@gmail.com>
2025-11-29 17:48:55 -0300

    chore: add create category endpoint

--- backend/my-inventory-app/src/main/java/com/bramworks/inventory/controller/CategoryController.java
+++ backend/my-inventory-app/src/main/java/com/bramworks/inventory/controller/CategoryController.java
@@ -5,11 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bramworks.inventory.model.Category;
 import com.bramworks.inventory.responses.ResponseRest;
 import com.bramworks.inventory.services.CategoryService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -29,4 +32,8 @@ public class CategoryController {
         return categoryService.getById(id);
     }
 
+    @PostMapping("/categories")
+    public ResponseEntity<ResponseRest> createCategory(@RequestBody Category category) {
+        return categoryService.create(category);
+    }
 }
